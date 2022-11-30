b2f4e1aa1e0db54598235a49d13dcfc121b290ca
Andrew Peter <andrewpeter2032@gmail.com>
2022-11-30 13:06:49 +0300

    prints alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""prints the ASCII alphabet, in lowercase, not followed by a new line except q,and e"""
+"""print alphabet, in lowercase,except q,and e"""
 
 for alphabet in range(97, 123):
     if alphabet != 101 and alphabet != 113:
