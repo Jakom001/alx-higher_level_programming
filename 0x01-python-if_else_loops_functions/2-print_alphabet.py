166b6bf957e34a393abca6ea77f0144c779f5571
Andrew Peter <andrewpeter2032@gmail.com>
2022-11-30 13:04:47 +0300

    print alphabet excluding others

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 """print alphabets in lowercase, not followed with a new line."""
-for alphabet("{}.format(chr(alphabet)), end="")
+for alphabet in range(97, 123):
+    print("{}".format(chr(alphabet)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 """prints the ASCII alphabet, in lowercase, not followed by a new line except q,and e"""
 
 for alphabet in range(97, 123):
-    if alphabet != 101 and 113:
+    if alphabet != 101 and alphabet != 113:
         print("{}".format(chr(alphabet)), end="")
