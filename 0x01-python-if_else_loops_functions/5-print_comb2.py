ae71fda0be4acc60038873affe3dbeb2baa628ec
Andrew Peter <andrewpeter2032@gmail.com>
2022-11-30 13:39:33 +0300

    print numbers between 0 and 99 with 2 digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,4 +2,7 @@
 """Write a program that prints numbers from 0 to 99"""
 
 for number in range(0, 100):
-    print("{:02}".format(number), end=", ")
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
