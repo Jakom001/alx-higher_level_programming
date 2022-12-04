e8ae6075525a08a7fea3b686534a40ecb09d8f07
Andrew Peter <andrewpeter2032@gmail.com>
2022-12-04 19:59:11 +0300

    print the largest number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return "None"
-    max_number = my_list[0]
-
-    for number in my_list:
-        if number > max_number:
-            max_number = number
-    return max_number
+    else:
+        max_number = my_list[0]
+        for number in my_list:
+            if number > max_number:
+                max_number = number
+        return max_number
