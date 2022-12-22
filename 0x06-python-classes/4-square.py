337a03460e80e061140ca9ee340de35aa0b14126
Andrew Peter <andrewpeter2032@gmail.com>
2022-12-22 05:17:55 +0300

    quare that defines a square using property and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
 
-"""Define a class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a sqaure"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """intialze a new square
-
+        """Initialize a new square.
         Args:
-            Size (int): size of the square
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """Retrieve the size"""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """set the value of size"""
         if not isinstance(value, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
     def area(self):
-        """return the area of the square"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
