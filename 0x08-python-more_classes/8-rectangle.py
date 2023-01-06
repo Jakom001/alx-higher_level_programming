47060d0bd7bc7980284a348cf607d731209fcf84
Andrew Peter <andrewpeter2032@gmail.com>
2023-01-06 04:55:31 +0300

    creatinga static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -4,16 +4,18 @@
 
 class Rectangle:
     """Represent a rectangle.
+
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
-        print_symbol (any): Symbol used for string representation
+        print_symbol (any): The symbol used for string representation.
     """
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -60,24 +62,25 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Return the Rectangle with greater area.
+        """Return the Rectangle with the greater area.
 
         Args:
-            rect_1 (Rectangle): The first rectangle
-            rect_2 (Rectangle): The second rectangle
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
+        Raises:
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
         """
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rec_1 must be an instance of Rectangle")
-
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-
-        if reac_1.area() >= react_2.area():
+        if rect_1.area() >= rect_2.area():
             return (rect_1)
-
+        return (rect_2)
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
+
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
@@ -85,7 +88,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
